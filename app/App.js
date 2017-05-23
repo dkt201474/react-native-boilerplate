2e72d594089de9bac25e87d86f2e9ce452b15923
dkt201474 <dkt201474pro@gmail.com>
2017-05-23 14:16:28 -0400

    our app support custom fonts now

--- app/App.js
+++ app/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Font } from 'expo';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 class App extends React.Component {
+  componentDidMount() {
+    Font.loadAsync({
+      'robotoLight': require('./assets/fonts/Roboto-Light.ttf'),
+      'robotoRegular': require('./assets/fonts/Roboto-Regular.ttf'),
+      'robotoCondensedRegular': require('./assets/fonts/RobotoCondensed-Regular.ttf'),
+      'robotoCondensedLight': require('./assets/fonts/RobotoCondensed-Light.ttf'),
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello </Text>
+        <Text style={{ fontFamily: 'robotoCondensedLight', fontSize: 30}}>Hello </Text>
       </View>
     );
   }
