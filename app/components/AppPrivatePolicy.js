6e6515a87ab0ff1097791b5bac0ec82b205211f0
dkt201474 <dkt201474pro@gmail.com>
2017-06-09 21:41:51 -0400

    AppPrivatePolicy ok

--- app/components/AppPrivatePolicy.js
+++ app/components/AppPrivatePolicy.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { gray } from '../theme/Colors';
 
 const AppPrivatePolicy = (
-  <View>
-    <Text>
+  <View style={{ paddingBottom: 20 }}>
+    <Text style={{ color: gray.g8 }}>
       ENANLY GROUP OF COMPANIES met tout en place pour que le respect de votre
       vie privée soit garanti. ENANLY GROUP OF COMPANIES S.A.R.L situé en
       République du Bénin a mis en place la présente politique de
@@ -13,39 +14,45 @@ const AppPrivatePolicy = (
       « données et informations personnelles » et à caractère non personnelles
       (comme définis ci-après) que vous fournissez au service ENANLY PAY.
 
-      \n\n«
-      Vous » désigne tout individu qui télécharge, accède, utilise, ou s’abonne
-      à
-      ENANLY PAY.\nLa présente « politique de confidentialité » ne régit que la
+      « Vous » désigne tout individu qui télécharge, accède, utilise, ou
+      s’abonne à
+      ENANLY PAY.
+
+      La présente « politique de confidentialité » ne régit que la
       collecte, le stockage, l’utilisation et la divulgation des données et
       informations collectées par ENANLY PAY. Elle n’est pas applicable aux
       données et informations recueillies par ENANLY GROUP par le biais de
-      tiers,
-      autres moyens, sites web ou services, et ne modifie et n’affecte pas aux
-      autres contrats présents ou ultérieurs, avec ENANLY GROUP. \nAinsi, si
-      ENANLY GROUP collecte des données vous concernant auprès de sources
-      tierces,
-      la collecte, le stockage, l’utilisation, et la divulgation des données et
+      tiers, autres moyens, sites web ou services, et ne modifie et n’affecte
+      pas aux
+      autres contrats présents ou ultérieurs, avec ENANLY GROUP. \
+
+      Ainsi, si ENANLY GROUP collecte des données vous concernant auprès de
+      sources
+      tierces, la collecte, le stockage, l’utilisation, et la divulgation des
+      données et
       informations seront régies par le contrat conclu par ENANLY GROUP avec «
       Vous » ou avec le tiers à l’origine de la source d’informations, sauf si
-      la
-      politique de confidentialité et protection de la vie privée du tiers
-      stipule
-      que la présente « politique de confidentialité » régira la collecte, le
+      la politique de confidentialité et protection de la vie privée du tiers
+      stipule que la présente « politique de confidentialité » régira la
+      collecte, le
       stockage, l’utilisation et la divulgation par ENANLY GROUP des données et
       informations rapportées par ce tiers.
 
-      \n\nDonnées et informations collectées
-      par ENANLY GROUP\nEn nous fournissant vos « Données et Informations
-      Personnelles », vous marquez votre accord avec notre« politique de
+      <Text style={{ fontSize: 15, fontWeight: 'bold', color: gray.g8 }}>
+        {'\n\n'}Données et informations collectées par ENANLY GROUP{'\n'}
+      </Text>
+      En nous fournissant vos « Données et Informations Personnelles », vous
+      marquez votre accord avec notre« politique de
       confidentialité » si vous n’acceptez pas cette « politique de
       confidentialité », veuillez ne pas fournir vos données personnelles. Cette
       «
       politique de confidentialité » fait partie intégrante des termes et
       conditions régissant l’utilisation de cette application.
 
-      \n\nDonnées et
-      informations personnelles.\nCi-après sont désignées les « Données et
+      <Text style={{ fontSize: 15, fontWeight: 'bold', color: gray.g8 }}>
+        {'\n\n'}Données et informations personnelles.{'\n'}
+      </Text>
+      Ci-après sont désignées les « Données et
       informations personnelles » comme les informations portant sur un individu
       identifiable. ENANLY GROUP collecte les données et informations quand vous
       les fournissez à ENANLY PAY (lors de votre création de compte, ajout et
@@ -61,19 +68,18 @@ const AppPrivatePolicy = (
       aura, au préalable, fait savoir au service clientèle ENANLY PAY que la
       confidentialité de son compte est compromise.
 
-      \n\nDonnées et informations
-      non personnelles\n Ci-après sont désignées « les données et informations
-      non
-      personnelle » collectées par ENANLY PAY comme des informations qui ne
+      <Text style={{ fontSize: 15, fontWeight: 'bold', color: gray.g8 }}>
+        {'\n\n'}Données et informations non personnelles{'\n'}
+      </Text>
+      Il s'agit des informations collectées par ENANLY PAY comme des
+      informations qui ne
       permettent pas d’identifier directement ou indirectement un individu,
-      comme
-      le sexe, la langue, l’indicatif téléphonique, l’identifiant unique
+      comme le sexe, la langue, l’indicatif téléphonique, l’identifiant unique
       d’appareil, la localisation, le fuseau horaire d’utilisation du produit,
-      le
-      pays de connexion à ENANLY PAY ainsi que les informations concernant les
+      le pays de connexion à ENANLY PAY ainsi que les informations concernant
+      les
       activités et la navigation des utilisateurs de ENANLY PAY, pour permettre
-      à
-      ENANLY PAY de mieux comprendre, améliorer, développer ses services,
+      à ENANLY PAY de mieux comprendre, améliorer, développer ses services,
       produits, et publicités. Si des « données et informations non
       personnelles »
       sont associées à des « données et informations personnelles »,
@@ -81,9 +87,11 @@ const AppPrivatePolicy = (
       de ces informations sera traitée comme des « données et informations
       personnelles » aussi longtemps qu’elles resteront associées.
 
-      \n\nUtilisation
-      des données et informations par ENANLY GROUP\nNous utiliserons vos données
-      pour les objectifs décrits ci-dessous avec votre consentement. Votre
+      <Text style={{ fontSize: 15, fontWeight: 'bold', color: gray.g8 }}>
+        {'\n\n'}Utilisation des données et informations par ENANLY GROUP{'\n'}
+      </Text>
+      Nous utiliserons vos données pour les objectifs décrits ci-dessous avec
+      votre consentement. Votre
       consentement est donné par votre téléchargement de l’application, création
       d’un compte(s), visite du service et par l’utilisation des services ou
       produits fournis par ENANLY PAY. ENANLY GROUP traite, collecte et utilise
@@ -106,7 +114,9 @@ const AppPrivatePolicy = (
       nos sites internet et application mobile. Option de refus : bien entendu,
       vous disposez d’un droit d’accès de modification, de rectification et de
       suppression des données qui vous concernent en contactant à tout moment
-      ENANLY GROUP.\nENANLY GROUP peut également traiter, accéder, utiliser,
+      ENANLY GROUP.
+
+      {'\n'}ENANLY GROUP peut également traiter, accéder, utiliser,
       stocker et divulguer des « données et informations personnelles » dans le
       but de détecter, parvenir et réparer toutes failles et sécurité,
       tentatives
@@ -132,8 +142,10 @@ const AppPrivatePolicy = (
       compter du jour de leur création jusqu’au jour de leur surpression, au
       travers d’une demande de votre part ou d’une autre raison.
 
-      \n\nTransfert à
-      un tiers des données et informations personnelles\nÀ l’exception des cas
+      <Text style={{ fontSize: 15, fontWeight: 'bold', color: gray.g8 }}>
+        {'\n\n'}Transfert à un tiers des données et informations personnelles{'\n'}
+      </Text>
+      À l’exception des cas
       précédemment citées « politique de confidentialité », ENANLY GROUP
       s’engage
       à ne pas divulguer vos « données et informations personnelles » vous
@@ -143,7 +155,9 @@ const AppPrivatePolicy = (
       GROUP peut être amenée à transmettre toutes « données et informations
       personnelles » à ses prestataires dans le but d’assurer certaines
       opérations
-      ou opérer à des services au nom d’ENANLY GROUP.\nL’hébergement des
+      ou opérer à des services au nom d’ENANLY GROUP.
+
+      {'\n'}L’hébergement des
       serveurs
       et l’envoi des emails, SMS, ou appels sont par exemple sous-traiter à des
       prestataires de services, garants de la protection des « données et
@@ -161,9 +175,11 @@ const AppPrivatePolicy = (
       d’activités (fusion-acquisition, joint-venture, vente d’actif, etc…),
       partenariats avec une autre marque, et autres finalités.
 
-      \n\nAccès,
-      modification, mises à jour et suppression des données et informations
-      personnelles\nVous pouvez accéder, copier, et modifier vos « données et
+      <Text style={{ fontSize: 15, fontWeight: 'bold', color: gray.g8 }}>
+        {'\n\n'}Accès, modification, mises à jour et suppression des données et
+        informations personnelles{'\n'}
+      </Text>
+      Vous pouvez accéder, copier, et modifier vos « données et
       informations personnelles », sous réserve qu’une disposition ou procédure
       légale n’obligent pas à les conserver ainsi, directement depuis le service
       en modifiant les informations fournies lors de la création du compte. Pour
@@ -173,16 +189,22 @@ const AppPrivatePolicy = (
       suppression du compte et l’interruption de l’envoi d’emails aux différents
       intervenants concernés se fera dans un délai maximal de sept (7) jours.
 
-      \n\nSécurité et cryptage des données personnelles\nENANLY GROUP s’engage à
-      protéger vos « données et informations personnelles » ENANLY GROUP et ses
+      <Text style={{ fontSize: 15, fontWeight: 'bold', color: gray.g8 }}>
+        {'\n\n'}Sécurité et cryptage des données personnelles{'\n'}
+      </Text>
+      ENANLY GROUP s’engage à protéger vos « données et informations
+      personnelles » ENANLY GROUP et ses
       prestataires, notamment des services d’hébergements, les partenaires
       télécoms et autres, pour ENANLY PAY ont recours à diverses technologies,
       contrôles et procédures de sécurité pour protéger vos « données et
       informations personnelles » contre des utilisations nuisibles ou non
       autorisées.
 
-      \n\nServices fournis par un tiers\nENANLY PAY peut proposer et
-      contenir des liens vers des sites web, produits ou services tiers. Les
+      <Text style={{ fontSize: 15, fontWeight: 'bold', color: gray.g8 }}>
+        {'\n\n'}Services fournis par un tiers{'\n'}
+      </Text>
+      ENANLY PAY peut proposer et contenir des liens vers des sites web,
+      produits ou services tiers. Les
       « données et informations personnelles » utilisées par ces tiers sont
       régies
       par les politiques de protection de la vie privée de ces tiers. Veuillez
@@ -190,10 +212,15 @@ const AppPrivatePolicy = (
       préalable les consulter avant de fournir des données personnelles à ces
       services tiers.
 
-      \n\nCookies et autres balises web\nLes sites web, services,
+      <Text style={{ fontSize: 15, fontWeight: 'bold', color: gray.g8 }}>
+        {'\n\n'}Cookies et autres balises web{'\n'}
+      </Text>
+      Les sites web, services,
       applications smartphones, emails, publicités, et tout autres supports
       numériques de ENANLY GROUP peuvent recourir à des « cookies » et autres
-      mécanismes tels que des cookies flash, et des balises web invisibles.\nUn
+      mécanismes tels que des cookies flash, et des balises web invisibles.{
+        '\n'
+      }Un
       « cookie » est un témoin de connexion qu’ENANLY PAY transfère et stocke
       vers
       l’ordinateur de visite pour disposer d’informations spécifiques. Ces
@@ -231,11 +258,13 @@ const AppPrivatePolicy = (
       veuillez
       consulter les préférences et paramètre de votre navigateur.
 
-      \n\nModification
-      de la politique de confidentialité et protection de la vie privée\nLa date
+      <Text style={{ fontSize: 15, fontWeight: 'bold', color: gray.g8 }}>
+        {'\n\n'}Modification
+        de la politique de confidentialité et protection de la vie privée{'\n'}
+      </Text>
+      La date
       d’effet de cette présente « politique de confidentialité »figure au haut
-      de
-      page. La date d’application sera modifiée à chaque mise à jour de la
+      de page. La date d’application sera modifiée à chaque mise à jour de la
       politique. ENANLY GROUP peut mettre à jour à tout moment cette « politique
       de confidentialité » et en modifiera la date d’application. Si la version
       mise à jour contient des changements relatifs aux « données et
@@ -247,7 +276,10 @@ const AppPrivatePolicy = (
       de
       votre part de sa nouvelle version.
 
-      \n\nContacter ENANLY PAY\nL’équipe ENANLY
+      <Text style={{ fontSize: 15, fontWeight: 'bold', color: gray.g8 }}>
+        {'\n\n'}Contacter ENANLY PAY{'\n'}
+      </Text>
+      L’équipe ENANLY
       PAY se tient à votre disposition pour toutes questions, remarques et
       commentaires en relation avec la présente « politique de confidentialité »
       d’ENANLY PAY. Veuillez, joindre l’équipe ENANLY PAY grâce aux services
